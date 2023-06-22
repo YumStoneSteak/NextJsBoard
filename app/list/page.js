f06aa6db0a49102d1debc04008bd0d68ff5eb3cb
YumStoneSteak <leedonghyuk2163@gmail.com>
2023-06-22 11:22:16 +0900

    Feat: list 영화 정보 출력
    - 제목, 연도, 감독, 장르, 러닝타임, 관람등급 출력
    - list css 수정

--- app/list/page.js
+++ app/list/page.js
@@ -1,26 +1,37 @@
 import { connectDB } from "@/util/database";
+import Image from "next/image";
 
 export default async function List() {
   let client = await connectDB;
   const db = client.db("sample_mflix");
-  let result = await db.collection("movies").find().toArray();
+  let movies = await db.collection("movies").find().toArray();
 
   return (
     <div className="list-bg">
-      {result.slice(2, 10).map((item, index) => {
-        return (
-          <div className="list-item" key={index}>
-            <img
-              src={`${result[index].poster}`}
-              alt={`/img/food${index}.png`}
-              width="50px"
-              height="auto"
-            />
-            <h4>{result[index].title}</h4>
-            <p>{result[index].plot}</p>
+      {movies.slice(0, 9).map((item, index) => (
+        <div className="list-item" key={index}>
+          <Image
+            src={`${movies[index].poster}`}
+            alt={index}
+            width="100"
+            height="162"
+            className="list-img"
+          />
+          <div className="list-desc">
+            <h4>{movies[index].title + " (" + movies[index].year + ")"}</h4>
+            <p>{movies[index].directors[0]}</p>
+            <p>
+              {movies[index].genres.join(", ") +
+                " | " +
+                movies[index].runtime +
+                " mins" +
+                " | " +
+                movies[index].rated}
+            </p>
+            <p>{movies[index].plot}</p>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 }

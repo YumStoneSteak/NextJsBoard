c1d5633a2dc46b1ba7efece4f1aae2bdbc208e54
YumStoneSteak <leedonghyuk2163@gmail.com>
2023-06-26 12:17:38 +0900

    Refactor: Chnage List to main
    - 메인페이지를 list로 변경

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
 import { connectDB } from "@/util/database";
 import { Collection, MongoClient } from "mongodb";
 
-export default async function Home() {
-  return <main>hello</main>;
+import LoadList from "./list/LoadList";
+
+export default function List() {
+  return (
+    <div className="list-bg">
+      <h1 className="main-title">DONGFLIX</h1>
+      <LoadList fromIndex={0} toIndex={20}></LoadList>
+    </div>
+  );
 }

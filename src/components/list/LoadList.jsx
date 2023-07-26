5d9be66c5b9769405f3326bbce09631ad0ba9fe9
Donghyuk Lee <leedonghyuk2163@gmail.com>
2023-07-26 17:04:06 +0900

    Feat: Add list-item-bg color
    - Add color-thief-react package
    - Refactor LoadList
    - Add list-item-bg color using color-thief
    - Modify transition timing

--- src/components/list/LoadList.jsx
+++ src/components/list/LoadList.jsx
@@ -1,13 +1,14 @@
 "use client";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
+import { useColor } from "color-thief-react";
 
-export default function LoadList(props) {
+const LoadList = (props) => {
   const [newMovies, setNewMovies] = useState([]);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const movies = JSON.parse(props.result);
     const updatedMovies = movies.map((movie, index) => ({
       id: movie._id.toString(),
@@ -27,69 +28,97 @@ export default function LoadList(props) {
     <SessionProvider>
       <div className="list-bg">
         {newMovies.map((movie, index) => (
-          <>
-            <div
-              className="list-item-blur"
-              key={index}
-              style={{
-                animationDelay: `${index * 20}ms`,
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundImage: `url(${movie.poster})`,
-                backgroundSize: "cover",
-                filter: "blur(5px)",
-                zIndex: "-1",
-              }}
-            />
-            <div
-              className="list-item"
-              key={index}
-              style={{
-                animationDelay: `${index * 20}ms`,
-              }}
-            >
-              <div className="relative">
-                <div className="overlay">
-                  <span
-                    className="overlay-black-span"
-                    onClick={() => alert("내 마음 속에 저장")}
-                    style={{ zIndex: "1" }}
-                  >
-                    ❤️
-                  </span>
-                </div>
-
-                <Image
-                  src={movie.poster}
-                  width={152}
-                  height={224}
-                  alt={index}
-                  className="list-img"
-                />
-              </div>
-
-              <div className="list-desc">
-                <Link href={"/detail/" + movie.id} className="Link">
-                  <h4>{movie.title + " (" + movie.year + ")"}</h4>
-                </Link>
-
-                <p>{movie.directors[0]}</p>
-                <p>
-                  {movie.genreList +
-                    " | " +
-                    movie.runtime +
-                    " mins | " +
-                    movie.rated}
-                </p>
-                <p>{movie.plot}</p>
-              </div>
-            </div>
-          </>
+          <MovieItem key={movie.id} movie={movie} index={index} />
         ))}
       </div>
     </SessionProvider>
   );
+};
+
+function getComplementaryColor(hexColor) {
+  // # 제거
+  const hex = hexColor.replace("#", "");
+
+  // R, G, B 값을 16진수에서 10진수로 변환
+  const r = parseInt(hex.substr(0, 2), 16);
+  const g = parseInt(hex.substr(2, 2), 16);
+  const b = parseInt(hex.substr(4, 2), 16);
+
+  // 보색 계산
+  const complementaryR = 255 - r;
+  const complementaryG = 255 - g;
+  const complementaryB = 255 - b;
+
+  // 클리핑
+  const complementaryRClipped = Math.max(0, Math.min(255, complementaryR));
+  const complementaryGClipped = Math.max(0, Math.min(255, complementaryG));
+  const complementaryBClipped = Math.max(0, Math.min(255, complementaryB));
+
+  // 10진수를 16진수로 변환하여 보색 색상 반환
+  const complementaryColor =
+    "#" +
+    ("00" + complementaryRClipped.toString(16)).slice(-2) +
+    ("00" + complementaryGClipped.toString(16)).slice(-2) +
+    ("00" + complementaryBClipped.toString(16)).slice(-2);
+
+  return complementaryColor;
 }
+
+const MovieItem = ({ movie, index }) => {
+  const Color = (poster) => {
+    const googleProxyURL =
+      "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=";
+
+    const { data, loading, error } = useColor(
+      googleProxyURL + encodeURIComponent(poster),
+      "hex",
+      {
+        crossOrigin: "anonymous",
+      }
+    );
+    return data;
+  };
+
+  // const otherColor = getComplementaryColor(data.toString());
+
+  return (
+    <div
+      className="list-item"
+      style={{
+        background: Color(movie.poster),
+        // color: otherColor,
+        animationDelay: `${index * 20}ms`,
+      }}
+    >
+      <div className="relativeHover">
+        <div className="overlay">
+          <span
+            className="overlay-black-span"
+            onClick={() => alert("내 마음 속에 저장")}
+          >
+            ❤️
+          </span>
+        </div>
+        <Image
+          src={movie.poster}
+          width={152}
+          height={224}
+          alt={index}
+          className="list-img"
+        />
+      </div>
+      <div className="list-desc">
+        <Link href={"/detail/" + movie.id} className="Link">
+          <h4>{movie.title + " (" + movie.year + ")"}</h4>
+        </Link>
+        <p>{movie.directors[0]}</p>
+        <p>
+          {movie.genreList + " | " + movie.runtime + " mins | " + movie.rated}
+        </p>
+        <p>{movie.plot}</p>
+      </div>
+    </div>
+  );
+};
+
+export default LoadList;
